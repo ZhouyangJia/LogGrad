0eb9b7737e80550a7d03d89fcfa6bf4af322a04d
ZhouyangJia <zhouyangjia@ZhouyangJiadeMacBook-Pro.local>
2016-09-07 08:48:32 +0800

    init

---
+++ src/FindLoggedSnippet.cpp
@@ -0,0 +1,335 @@
+//
+//  FindLoggedSnippet.cpp
+//  LLVM
+//
+//  Created by ZhouyangJia on 16/4/13.
+//
+//
+
+#include "LogGrad.h"
+#include "FindLoggedSnippet.h"
+
+extern string lastName;
+extern int fileTotalNum;
+extern int fileNum;
+
+
+extern map<string, int> myLoggedTime;
+extern map<string, int> myCalledTime;
+
+
+///get the source code of stmt
+StringRef FindLoggedVisitor::expr2str(Stmt *s){
+    
+    if(!s)
+        return "";
+    
+    FullSourceLoc begin = CI->getASTContext().getFullLoc(s->getLocStart());
+    FullSourceLoc end = CI->getASTContext().getFullLoc(s->getLocEnd());
+    
+    if(begin.isInvalid() || end.isInvalid())
+        return "";
+    
+    SourceRange sr(begin.getExpansionLoc(), end.getExpansionLoc());
+    
+    return Lexer::getSourceText(CharSourceRange::getTokenRange(sr), CI->getSourceManager(), LangOptions(), 0);
+}
+
+
+bool FindLoggedVisitor::hasKeyWord(string name){
+    
+    transform(name.begin(), name.end(), name.begin(), ::tolower);  //::toupper
+    
+    //string* spilted;
+    //spilted = spiltWord(name);
+    
+    string word[30] = {
+        "log",
+        "error",
+        "err",
+        "die",
+        "fail",
+        
+        "hint",
+        "put",
+        "assert",
+        "trace",
+        "print",
+        
+        "write",
+        "report",
+        "record",
+        "dump",
+        "msg",
+        
+        "message",
+        "out",
+        "warn",
+        "debug"
+        "emerg"
+        
+        "alert"
+        "crit"
+    };
+    
+    for(int i = 0; i < 30; i++){
+        if(word[i].length() == 0)
+            break;
+        if(name.find(word[i]) < name.length() && (name.find_last_of(':') == string::npos || name.find(word[i]) > name.find_last_of(':')))
+            return true;
+    }
+    
+    return false;
+}
+
+
+
+///search log site in the subtree of stmt
+CallExpr* FindLoggedVisitor::searchLog(Stmt *stmt){
+    
+    if(!stmt)
+        return 0;
+    
+    if(CallExpr *callExpr = dyn_cast<CallExpr>(stmt)){
+        if(FunctionDecl *functionDecl = callExpr->getDirectCallee()){
+            StringRef callName = functionDecl->getQualifiedNameAsString();
+            
+            if(hasKeyWord(callName)){
+                return callExpr;
+            }
+        }
+    }
+    
+    for(Stmt::child_iterator it = stmt->child_begin(); it != stmt->child_end(); ++it){
+        if(Stmt *child = *it){
+            CallExpr *ret = searchLog(child);
+            if(ret)
+                return ret;
+        }
+    }
+    
+    return 0;
+}
+
+
+///search call site in the subtree of stmt
+CallExpr* FindLoggedVisitor::searchCall(Stmt *stmt){
+    
+    if(!stmt)
+        return 0;
+    
+    if(CallExpr *callExpr = dyn_cast<CallExpr>(stmt)){
+        if(callExpr->getDirectCallee()){
+            return callExpr;
+        }
+    }
+    
+    for(Stmt::child_iterator it = stmt->child_begin(); it != stmt->child_end(); ++it){
+        if(Stmt *child = *it){
+            CallExpr *ret = searchCall(child);
+            if(ret)
+                return ret;
+        }
+    }
+    
+    return 0;
+}
+
+
+///search the function body for call-log pairs
+void FindLoggedVisitor::travelStmt(Stmt *stmt, Stmt *father){
+    
+    if(!stmt || !father)
+        return;
+    
+    if(CallExpr* callExpr = dyn_cast<CallExpr>(stmt)){
+        StringRef callName = "";
+        if(FunctionDecl *functionDecl = callExpr->getDirectCallee())
+            callName = functionDecl->getQualifiedNameAsString();
+        myCalledTime[callName]++;
+    }
+
+    ///deal with first log pattern
+    //code
+    //  if(foo())
+    //      log();
+    //\code
+    
+    ///find if(foo()), stmt is located in the condexpr of ifstmt
+    if(IfStmt *ifStmt = dyn_cast<IfStmt>(father)){
+        if(ifStmt->getCond() == stmt){
+            
+            ///search for call in condexpr
+            if((myCallExpr = searchCall(stmt))){
+                StringRef callName = "";
+                if(FunctionDecl *functionDecl = myCallExpr->getDirectCallee())
+                    callName = functionDecl->getQualifiedNameAsString();
+                
+                ///search for log in both 'then body' and 'else body'
+                if((myLogExpr = searchLog(ifStmt->getThen()))){
+                    myLoggedTime[callName]++;
+                }
+                else if((myLogExpr = searchLog(ifStmt->getElse()))){
+                    myLoggedTime[callName]++;
+                }
+
+                ///do not search inside the condexpr
+                //return;
+            }
+        }
+    }
+    
+    ///deal with second log pattern
+    //code
+    //  switch(foo());
+    //      ...
+    //      log();
+    //\code
+    
+    ///find switch(foo()), stmt is located in the condexpr of switchstmt
+    if(SwitchStmt *switchStmt = dyn_cast<SwitchStmt>(father)){
+        if(switchStmt->getCond() == stmt){
+            
+            ///search for call in condexpr
+            if((myCallExpr = searchCall(stmt))){
+                StringRef callName = "";
+                if(FunctionDecl *functionDecl = myCallExpr->getDirectCallee())
+                    callName = functionDecl->getQualifiedNameAsString();
+                
+                ///search for log in each 'switch body'
+                if((myLogExpr = searchLog(switchStmt->getBody()))){
+                    myLoggedTime[callName]++;
+                }
+                ///do not search inside the condexpr
+                //return;
+            }
+        }
+    }
+    
+    ///deal with third log pattern
+    //code
+    //  ret = foo();
+    //  if(ret)
+    //      log();
+    //\code
+    
+    ///find 'ret = foo()', when stmt = 'BinaryOperator', op == '=' and RHS == 'callexpr'
+    if(BinaryOperator *binaryOperator = dyn_cast<BinaryOperator>(stmt)){
+        if(binaryOperator->getOpcode() == BO_Assign){
+            if(Expr *expr = binaryOperator->getRHS()){
+                expr = expr->IgnoreImpCasts();
+                expr = expr->IgnoreImplicit();
+                expr = expr->IgnoreParens();
+                if(CallExpr *callExpr = dyn_cast<CallExpr>(expr)){
+                    
+                    myReturnName = expr2str(binaryOperator->getLHS());
+                    if(myReturnName == "")
+                        return;
+                    
+                    myCallExpr = callExpr;
+                    StringRef callName = "";
+                    if(FunctionDecl *functionDecl = myCallExpr->getDirectCallee())
+                        callName = functionDecl->getQualifiedNameAsString();
+                    
+                    ///search for brothers of stmt
+                    ///young_brother means the branch after stmt
+                    ///we only search for branches after stmt
+                    bool isYoungBrother = false;
+                    for(Stmt::child_iterator bro = father->child_begin(); bro != father->child_end(); ++bro){
+                        
+                        if(Stmt *brother = *bro){
+                            
+                            if(brother == stmt){
+                                isYoungBrother = true;
+                                continue;
+                            }
+                            
+                            if(isYoungBrother == false){
+                                continue;
+                            }
+                            
+                            ///find 'if(ret)'
+                            if(IfStmt *ifStmt = dyn_cast<IfStmt>(brother)){
+                                
+                                ///the return name, which is checked in condexpr
+                                StringRef myCondStr = expr2str(ifStmt->getCond());
+                                if(myCondStr.find(myReturnName) != string::npos){
+                                    
+                                    ///search for log in both 'then body' and 'else body'
+                                    if((myLogExpr = searchLog(ifStmt->getThen()))){
+                                        myLoggedTime[callName]++;
+                                        break;
+                                    }
+                                    else if((myLogExpr = searchLog(ifStmt->getElse()))){
+                                        myLoggedTime[callName]++;
+                                        break;
+                                    }
+                                }
+                            }
+                            
+                        }
+                    }//end for
+                    //return;
+                }
+            }
+        }
+    }
+    
+    for(Stmt::child_iterator it = stmt->child_begin(); it != stmt->child_end(); ++it){
+        if(Stmt *child = *it)
+            travelStmt(child, stmt);
+    }
+    
+    return;
+}
+
+
+bool FindLoggedVisitor::VisitFunctionDecl (FunctionDecl* Declaration){
+    
+    if(!Declaration)
+        return true;
+    
+    if(!(Declaration->isThisDeclarationADefinition() && Declaration->hasBody()))
+        return true;
+    
+    FullSourceLoc FullLocation = CI->getASTContext().getFullLoc(Declaration->getLocStart());
+    
+    if(!FullLocation.isValid())
+        return true;
+    
+    ///the function is located in head file
+    if(FullLocation.getExpansionLoc().getFileID() != CI->getSourceManager().getMainFileID())
+        return true;
+    
+    //llvm::errs()<<"Found function "<<Declaration->getQualifiedNameAsString() ;
+    //llvm::errs()<<" @ " << FullLocation.printToString(FullLocation.getManager()) <<"\n";
+    
+    
+    hasRecorded.clear();
+    if(Stmt* function = Declaration->getBody()){
+        travelStmt(function, function);
+    }
+    
+    return true;
+}
+
+
+void FindLoggedConsumer::HandleTranslationUnit(ASTContext& Context) {
+    
+    Visitor.TraverseDecl(Context.getTranslationUnitDecl());
+    return;
+}
+
+
+std::unique_ptr<clang::ASTConsumer> FindLoggedAction::CreateASTConsumer(CompilerInstance& Compiler, StringRef InFile){
+
+    if(InFile == lastName)
+        return 0;
+    else{
+        llvm::errs()<<"["<<++fileNum<<"/"<<fileTotalNum<<"]"<<" Now analyze the file: "<<InFile<<"\n";
+        lastName = InFile;
+        Rewriter rewriter;
+        rewriter.setSourceMgr(Compiler.getSourceManager(), Compiler.getLangOpts());
+        return std::unique_ptr<clang::ASTConsumer>(new FindLoggedConsumer(&Compiler, InFile));
+    }
+}
---
+++ src/LogGrad.cpp
@@ -0,0 +1,100 @@
+//
+//  LogGrad.cpp
+//  LLVM
+//
+//  Created by ZhouyangJia on 16/4/13.
+//
+//
+
+#include "LogGrad.h"
+#include "FindLoggedSnippet.h"
+
+
+// Shared variables
+// count files
+string lastName;
+int fileNum;
+int fileTotalNum;
+
+FILE* fFuncRuleModel;
+
+// Function rule mining
+map<string, int> myLoggedTime;
+map<string, int> myCalledTime;
+
+
+static cl::extrahelp CommonHelp(CommonOptionsParser::HelpMessage);
+
+static cl::extrahelp MoreHelp(
+	"\tFor example, to run clang-smartlog on all files in a subtree of the\n"
+	"\tsource tree, use:\n"
+	"\n"
+	"\t  find path/in/subtree -name '*.cpp'|xargs clang-smartlog\n"
+	"\n"
+	"\tor using a specific build path:\n"
+	"\n"
+	"\t  find path/in/subtree -name '*.cpp'|xargs clang-loggrad -p build/path\n"
+	"\n"
+	"\tNote, that path/in/subtree and current directory should follow the\n"
+	"\trules described above.\n"
+	"\n"
+);
+
+static cl::OptionCategory ClangMytoolCategory("clang-loggrad options");
+
+static std::unique_ptr<opt::OptTable> Options(createDriverOptTable());
+
+
+static cl::opt<bool> FindLoggedSnippet("log-grad",
+                                       cl::desc("Calculate log grad."),
+                                       cl::cat(ClangMytoolCategory));
+
+
+int main(int argc, const char **argv) {
+	
+	CommonOptionsParser OptionsParser(argc, argv, ClangMytoolCategory);
+	vector<string> source = OptionsParser.getSourcePathList();
+    fileTotalNum = source.size();
+    
+    std::unique_ptr<FrontendActionFactory> FrontendFactory;
+ 
+    if(FindLoggedSnippet){
+        
+        bool diag = true;
+        fileNum = 0;
+        lastName = "";
+        myLoggedTime.clear();
+        myCalledTime.clear();
+        
+        FrontendFactory = newFrontendActionFactory<FindLoggedAction>();
+        
+        fFuncRuleModel = fopen("function_rule_model.out","w");
+        for(unsigned i = 0; i < source.size(); i++){
+            vector<string> mysource;
+            mysource.push_back(source[i]);
+            ClangTool Tool(OptionsParser.getCompilations(), mysource);
+            Tool.clearArgumentsAdjusters();
+            if(diag){IgnoringDiagConsumer* idc = new IgnoringDiagConsumer();
+                Tool.setDiagnosticConsumer(idc);}
+            Tool.run(FrontendFactory.get());
+        }
+        
+        map<string,int>::iterator it;
+        for(it = myCalledTime.begin(); it != myCalledTime.end(); ++it){
+            
+            string name = it->first;
+            if(name == "")
+                continue;
+            int call = it->second;
+            int log = myLoggedTime[name];
+            char arg[100];
+            sprintf(arg, "%s,%d,%d\n", name.c_str(), call, log);
+            fputs(arg,fFuncRuleModel);
+            
+        }
+        
+        fclose(fFuncRuleModel);
+    }
+    
+	return 0;
+}
